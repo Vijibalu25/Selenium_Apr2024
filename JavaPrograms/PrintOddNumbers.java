e70d851a44d3e5216536385ac97c73e75be41a73
Vijibalu25 <140432231+Vijibalu25@users.noreply.github.com>
2024-04-28 17:18:33 +0530

    Add files via upload

---
+++ JavaPrograms/PrintOddNumbers.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class PrintOddNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Logic to find Odd Numbers:  ");
+		for (int i=0; i<=10; i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println(i);
+			}
+		}
+}
+}
